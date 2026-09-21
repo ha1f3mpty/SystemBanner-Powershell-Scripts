cf3a8bfac76402e71c2b6ded8ebf944ccdc2a243
ha1f3mpty <ha1f3mpty@gmail.com>
2026-09-21 10:50:19 -0600

    Delete Deployment/PowerShell/Detect_RemediateScripts directory

--- Deployment/PowerShell/Detect_RemediateScripts/systemBannerInstall-Detect.ps1
+++
@@ -1,9 +0,0 @@
-$exePath = "C:\Program Files\SystemBanner\SystemBanner.exe"
-
-if (Test-Path -LiteralPath $exePath -PathType Leaf) {
-    Write-Host "SystemBanner is installed."
-    exit 0
-}
-
-Write-Host "SystemBanner is not installed."
-exit 1
